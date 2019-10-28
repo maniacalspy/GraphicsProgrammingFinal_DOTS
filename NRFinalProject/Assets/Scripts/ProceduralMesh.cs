1ca6c05922e8e64474467ce617d9c562ac2f010c
Nolan Roher <maniacalspy@gmail.com>
2019-10-28 10:57:12 -0500

    Adds logic to change mesh over time

--- NRFinalProject/Assets/Scripts/ProceduralMesh.cs
+++ NRFinalProject/Assets/Scripts/ProceduralMesh.cs
@@ -7,7 +7,8 @@ public class ProceduralMesh : MonoBehaviour
 {
     Mesh mesh;
 
-
+    float QuadExtents;
+    int buffer;
     enum MeshShapes { triangle, square, quad, loop};
 
     [SerializeField]
@@ -20,6 +21,8 @@ public class ProceduralMesh : MonoBehaviour
     private void Awake()
     {
         mesh = GetComponent<MeshFilter>().mesh;
+        QuadExtents = 1.0f;
+        buffer = 0;
     }
     // Start is called before the first frame update
     void Start()
@@ -33,7 +36,7 @@ public class ProceduralMesh : MonoBehaviour
                 MakeSquareMeshData(1);
                 break;
             case (MeshShapes.quad):
-                MakeQuadMeshData();
+                MakeQuadMeshData(1);
                 break;
             case (MeshShapes.loop):
                 MakeLoopMeshData();
@@ -49,28 +52,43 @@ public class ProceduralMesh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (thisShape == MeshShapes.quad)
+        {
+            buffer++;
+            //Debug.Log(QuadExtents);
+            if ( buffer == 10)
+            {
+                QuadExtents += .05f;
+                MakeQuadMeshData(QuadExtents);
+                CreateMesh();
+                buffer = 0;
+            }
+        }
     }
 
     public void MakeTriangleMeshData()
     {
+        thisShape = MeshShapes.triangle;
         verticies = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0)};
         triangles = new int[] { 0, 1, 2 };
     }
     public void MakeSquareMeshData(float xextents)
     {
+        thisShape = MeshShapes.square;
         verticies = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, xextents), new Vector3(xextents, 0, 0), new Vector3(xextents,0,xextents)};
         triangles = new int[] { 0, 1, 2, 2, 1, 3 };
     }
 
-    public void MakeQuadMeshData()
+    public void MakeQuadMeshData(float extents)
     {
-        verticies = new Vector3[] { new Vector3(0,0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(1,0,1), //bottom face
-                                    new Vector3(0,0,0), new Vector3(0,1,0), new Vector3 (1,0,0), new Vector3 (1,1,0), //front face
-                                    new Vector3(0,1,0), new Vector3(0,1,1), new Vector3(1,1,0), new Vector3(1,1,1), //top face
-                                    new Vector3(0,0,1), new Vector3(1,0,1), new Vector3(0,1,1), new Vector3(1,1,1), //back face
-                                    new Vector3(0,0,0), new Vector3(0,0,1), new Vector3(0,1,0), new Vector3(0,1,1), //left face
-                                    new Vector3(1,0,0), new Vector3(1,1,0), new Vector3(1,0,1), new Vector3(1,1,1) //right face
+        thisShape = MeshShapes.quad;
+        this.QuadExtents = extents;
+        verticies = new Vector3[] { new Vector3(0,0, 0), new Vector3(extents, 0, 0), new Vector3(0, 0, extents), new Vector3(extents,0,extents), //bottom face
+                                    new Vector3(0,0,0), new Vector3(0,extents,0), new Vector3 (extents,0,0), new Vector3 (extents,extents,0), //front face
+                                    new Vector3(0,extents,0), new Vector3(0,extents,extents), new Vector3(extents,extents,0), new Vector3(extents,extents,extents), //top face
+                                    new Vector3(0,0,extents), new Vector3(extents,0,extents), new Vector3(0,extents,extents), new Vector3(extents,extents,extents), //back face
+                                    new Vector3(0,0,0), new Vector3(0,0,extents), new Vector3(0,extents,0), new Vector3(0,extents,extents), //left face
+                                    new Vector3(extents,0,0), new Vector3(extents,extents,0), new Vector3(extents,0,extents), new Vector3(extents,extents,extents) //right face
         };
                                     
         triangles = new int[] { 0, 1, 2, 2,1,3,
@@ -84,6 +102,7 @@ public class ProceduralMesh : MonoBehaviour
 
     public void MakeLoopMeshData()
     {
+        thisShape = MeshShapes.loop;
         List<Vector3> NewVerticies = new List<Vector3>();
         List<int> newTriangles = new List<int>();
         for (int i = 1; i < 6; i++)
